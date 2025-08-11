dd575d585b2cb0023859212caed64ad67c628a3a
Jethro Cadang <jethrocadang@gmail.com>
2025-08-11 18:50:02 +0800

    config: added scafholding for express

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 //app.js
 
 const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
 const app = express();
 const port = process.env.PORT || 3000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("hello world!");
 });
 
 app.listen(port, () => {
-    console.log(`Project Zero Express: Listening on port ${port}`)
-})
+  console.log(`Project Zero Express: Listening on port ${port}`);
+});
