bb8bbd2b123678163175676daf60ef965fff467a
Jethro Cadang <jethrocadang@gmail.com>
2025-08-12 23:19:53 +0800

    feat: RBAC Models

---
+++ models/Permission.js
@@ -0,0 +1,19 @@
+//models/Permission.js
+
+const mongoose = require("mongoose");
+
+const PermissionSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    description: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Permission", PermissionSchema);
---
+++ models/User.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const UserSchema = mongoose.Schema(
+  {
+    firstName: { type: String, required: true, trim: true },
+    lastName: { type: String, required: true, trim: true },
+    email: {
+      type: String,
+      unique: true,
+      match: [/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/],
+    },
+    password: { type: String, required: true, minlength: 8, select: false },
+    roles: [{ type: mongoose.Schema.Types.ObjectId, ref: "Role" }],
+    permissions: [{ type: mongoose.Schema.Types.ObjectId, ref: "Permission" }],
+    isActive: { type: Boolean, default: true },
+    lastLoginAt: { type: Date },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("User", UserSchema);
